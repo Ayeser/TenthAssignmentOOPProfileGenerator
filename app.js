90cc9fb5b5694a72bb7d7e918dfa36cc4635adcc
Matthew Wester <MatthewWester@live.com>
2020-02-07 00:54:14 -0700

    Made final corrections to app.js

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const fs = require("fs");
 var managerCards = [];
 var engineerCards = [];
 var internCards = [];
-var finalDirectory = [];
 
 function collectTeamInfo() {
     inquirer
@@ -73,20 +72,8 @@ function askIfMoreEmployees() {
             collectTeamInfo();
         } else {
             console.log("Very well then, that team is done");
-            inquirer
-            .prompt([{type:"list", message:"Do you have an additional team to enter into this company's directory?", choices:["yes", "no"], name:"newTeam"}])
-            .then(function (newTeamResponse) {
-                if(newTeamResponse.newTeam === "yes") {
-                    exportToHTML();
-                    clearTeamInfo();
-                    collectTeamInfo();
-                } else {
-                    console.log("Your directory is now complete.");
-                    exportToHTML();
-                    createDirectoryFinal();
-                    clearTeamInfo();
-                }
-            })
+            createDirectoryFinal();
+            clearTeamInfo();
         }
     })
 }
@@ -97,32 +84,18 @@ function clearTeamInfo() {
     internCards = [];
 }
 
-function exportToHTML() {
-    teamComplete = [];
-    if (managerCards.length > 0) {
-        for(j=0;j<managerCards.length;j++) {
-            teamComplete.push(managerCards[j]);
-        }
-    }
-    if (engineerCards.length > 0){
-        for(k=0;k<engineerCards.length;k++) {
-            teamComplete.push(engineerCards[k]);
-        }
-    }
-    if (internCards.length > 0) {
-        for(l=0;l<internCards.length;l++) {
-            teamComplete.push(internCards[l]);
-        }
-    }
-    for (m=0;m<teamComplete.length;m++) {
-        finalDirectory.push(teamComplete[m]);
-    }
-}
-
 function createDirectoryFinal() {
-    var stringFinalDirectory = ``;
-    for (i=0;i<finalDirectory.length;i++) {
-        stringFinalDirectory += `<div class="card" style="width: 18rem;"><div class="card-body"><h5 class="card-title"> ${JSON.stringify(finalDirectory[i])} </h5><h6 class="card-subtitle mb-2 text-muted"> ${JSON.stringify(finalDirectory[i].name)} </h6><p> ${JSON.stringify(finalDirectory[i].id)}</p><p>${JSON.stringify(finalDirectory[i].email)}</p><p>${JSON.stringify(finalDirectory[i][3])}</p>`;
+    var managerString = ``;
+    for (i=0;i<managerCards.length;i++) {
+        managerString += `<div class="card" style="width: 18rem;"><div class="card-body"><h5 class="card-title">Manager</h5><h6 class="card-subtitle mb-2 text-muted"> Name: ${managerCards[i].name} </h6><p>Employee ID: ${managerCards[i].id}</p><p>E-mail: ${managerCards[i].email}</p><p>Office Number: ${managerCards[i].officeNumber}</p>`;
+    };
+    var engineerString = ``;
+    for (j=0;i<engineerCards.length;j++) {
+        engineerString += `<div class="card" style="width: 18rem;"><div class="card-body"><h5 class="card-title">Engineer</h5><h6 class="card-subtitle mb-2 text-muted"> Name: ${engineerCards[j].name} </h6><p>Employee ID: ${engineerCards[j].id}</p><p>E-mail: ${engineerCards[j].email}</p><p>Github Username: ${engineerCards[j].github}</p>`;
+    };
+    var internString = ``;
+    for (k=0;k<internCards.length;k++) {
+        engineerString += `<div class="card" style="width: 18rem;"><div class="card-body"><h5 class="card-title">Intern</h5><h6 class="card-subtitle mb-2 text-muted"> Name: ${internCards[k].name} </h6><p>Employee ID: ${internCards[k].id}</p><p>E-mail: ${internCards[k].email}</p><p>School: ${internCards[k].school}</p>`;
     };
     const directory = `<!DOCTYPE html>
     <html lang="en">
@@ -136,7 +109,7 @@ function createDirectoryFinal() {
           <title>Company Directory</title>
           <nav class="navbar navbar-light bg-light">
           <span class="navbar-brand mb-0 h1">Company Directory</span>
-          </nav>` + stringFinalDirectory + `<script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script><script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js' integrity='sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1' crossorigin='anonymous'></script><script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js' integrity='sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM' crossorigin='anonymous'></script></body><footer class='wrapper'></footer></html>`;
+          </nav>` + managerString + engineerString + internString + `<script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script><script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js' integrity='sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1' crossorigin='anonymous'></script><script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js' integrity='sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM' crossorigin='anonymous'></script></body><footer class='wrapper'></footer></html>`;
     console.log(directory);
     fs.appendFile("./output/directory.html", directory, err => {
         if(err) {
