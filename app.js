52ed6f80c33b7bb0c6fcceeeeb8b651d10daceaa
Matthew Wester <MatthewWester@live.com>
2020-02-06 23:53:04 -0700

    Company Directory populates but imperfectly

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const inquirer = require("inquirer");
-const Employee = require("./lib/Employee.js");
 const Engineer = require("./lib/Engineer.js");
 const Intern = require("./lib/Intern.js");
 const Manager = require("./lib/Manager.js");
+const fs = require("fs");
 
 var managerCards = [];
 var engineerCards = [];
 var internCards = [];
-
-function createStartOfHTML() {
-    //create start of file
-}
+var finalDirectory = [];
 
 function collectTeamInfo() {
     inquirer
@@ -86,7 +83,7 @@ function askIfMoreEmployees() {
                 } else {
                     console.log("Your directory is now complete.");
                     exportToHTML();
-                    finalHTMLPiece();
+                    createDirectoryFinal();
                     clearTeamInfo();
                 }
             })
@@ -101,24 +98,54 @@ function clearTeamInfo() {
 }
 
 function exportToHTML() {
-    teamComplete = []
+    teamComplete = [];
     if (managerCards.length > 0) {
-        teamComplete.push(managerCards);
+        for(j=0;j<managerCards.length;j++) {
+            teamComplete.push(managerCards[j]);
+        }
     }
     if (engineerCards.length > 0){
-        teamComplete.push(engineerCards);
+        for(k=0;k<engineerCards.length;k++) {
+            teamComplete.push(engineerCards[k]);
+        }
     }
     if (internCards.length > 0) {
-        teamComplete.push(internCards);
+        for(l=0;l<internCards.length;l++) {
+            teamComplete.push(internCards[l]);
+        }
     }
-    console.log("The team you just entered consists of: " + teamComplete);
+    for (m=0;m<teamComplete.length;m++) {
+        finalDirectory.push(teamComplete[m]);
+    }
 }
 
-function finalHTMLPiece() {
-    //add final piece of HTML
+function createDirectoryFinal() {
+    var stringFinalDirectory = ``;
+    for (i=0;i<finalDirectory.length;i++) {
+        stringFinalDirectory += `<div class="card" style="width: 18rem;"><div class="card-body"><h5 class="card-title"> ${JSON.stringify(finalDirectory[i])} </h5><h6 class="card-subtitle mb-2 text-muted"> ${JSON.stringify(finalDirectory[i].name)} </h6><p> ${JSON.stringify(finalDirectory[i].id)}</p><p>${JSON.stringify(finalDirectory[i].email)}</p><p>${JSON.stringify(finalDirectory[i][3])}</p>`;
+    };
+    const directory = `<!DOCTYPE html>
+    <html lang="en">
+       <head>
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
+          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+          <link href="https://fonts.googleapis.com/css?family=BioRhyme|Cabin&display=swap" rel="stylesheet">
+          <title>Company Directory</title>
+          <nav class="navbar navbar-light bg-light">
+          <span class="navbar-brand mb-0 h1">Company Directory</span>
+          </nav>` + stringFinalDirectory + `<script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script><script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js' integrity='sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1' crossorigin='anonymous'></script><script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js' integrity='sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM' crossorigin='anonymous'></script></body><footer class='wrapper'></footer></html>`;
+    console.log(directory);
+    fs.appendFile("./output/directory.html", directory, err => {
+        if(err) {
+            console.log(err);
+        }
+        console.log("Success!");
+    });
 }
 
-createStartOfHTML();
 collectTeamInfo();
 
 
