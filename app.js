6ce8047c571f84a3a851e634414f8c6679336b5e
Matthew Wester <MatthewWester@live.com>
2020-02-08 17:12:50 -0700

    Placed variables outside for loops in app.js file and now works properly

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const fs = require("fs");
 var managerCards = [];
 var engineerCards = [];
 var internCards = [];
+var managerString = "";
+var engineerString = ``;
+var internString = ``;
 
 function collectTeamInfo() {
     inquirer
@@ -36,9 +39,9 @@ function collectTeamInfo() {
         .then(function (response) {
             if(response.role === "Manager"){
                 inquirer.prompt([{type:"number", message:"What is this manager's office number?", name: "officeNumber"}])
-                .then(function (res) {
+                .then(function (respo) {
                     console.log("Manager " + response.username + " is now entered into the system.");
-                    managerCards.push(new Manager(response.username, response.id, response.email, res.officeNumber));
+                    managerCards.push(new Manager(response.username, response.id, response.email, respo.officeNumber));
                     console.log(managerCards);
                     askIfMoreEmployees();
                 })
@@ -46,18 +49,18 @@ function collectTeamInfo() {
 
             if(response.role === "Engineer"){
                 inquirer.prompt([{type:"input", message:"What is this engineer's github?", name: "github"}])
-                .then(function (res) {
+                .then(function (respon) {
                     console.log("Engineer " + response.username + " is now entered into the system.");
-                    engineerCards.push(new Engineer(response.username, response.id, response.email, res.github));
+                    engineerCards.push(new Engineer(response.username, response.id, response.email, respon.github));
                     askIfMoreEmployees();
                 })
             }
 
             if (response.role === "Intern") {
                 inquirer.prompt([{type:"input", message:"What is this intern's school?", name: "school"}])
-                .then(function (res) {
+                .then(function (respond) {
                     console.log("Intern " + response.username + " is now entered into the system.");
-                    internCards.push(new Intern(response.username, response.id, response.email, res.school));
+                    internCards.push(new Intern(response.username, response.id, response.email, respond.school));
                     askIfMoreEmployees();
                 })
             }
@@ -66,37 +69,37 @@ function collectTeamInfo() {
 
 function askIfMoreEmployees() {
     inquirer
-    .prompt([{type:"list", message:"Do you have more employees to enter into this team?", choices:["yes", "no"], name:"more"}])
-    .then(function (responsive) {
-        if(responsive.more === "yes") {
+    .prompt([{type:"list", message:"Do you have more employees to enter into this team?", choices:["yes", "no"], name: "emplore"}])
+    .then(function (resp) {
+        if(resp.emplore === "yes") {
             collectTeamInfo();
         } else {
             console.log("Very well then, that team is done");
             createDirectoryFinal();
-            clearTeamInfo();
         }
     })
 }
 
-function clearTeamInfo() {
-    managerCards = [];
-    engineerCards = [];
-    internCards = [];
-}
-
 function createDirectoryFinal() {
-    var managerString = ``;
+    if (managerCards.length > 0) {
     for (i=0;i<managerCards.length;i++) {
-        managerString += `<div class="card" style="width: 18rem;"><div class="card-body"><h5 class="card-title">Manager</h5><h6 class="card-subtitle mb-2 text-muted"> Name: ${managerCards[i].name} </h6><p>Employee ID: ${managerCards[i].id}</p><p>E-mail: ${managerCards[i].email}</p><p>Office Number: ${managerCards[i].officeNumber}</p>`;
+        console.log(managerCards);
+        managerString += "<div class='card' style='width: 18rem;'><div class='card-body'><h5 class='card-title'>Manager</h5><h6 class='card-subtitle mb-2 text-muted'> Name: " + managerCards[i].name + "</h6><p>Employee ID: " + managerCards[i].id + "</p><p>E-mail: " + managerCards[i].email + "</p><p>Office Number: " + managerCards[i].officeNumber + "</p>";
     };
-    var engineerString = ``;
-    for (j=0;i<engineerCards.length;j++) {
-        engineerString += `<div class="card" style="width: 18rem;"><div class="card-body"><h5 class="card-title">Engineer</h5><h6 class="card-subtitle mb-2 text-muted"> Name: ${engineerCards[j].name} </h6><p>Employee ID: ${engineerCards[j].id}</p><p>E-mail: ${engineerCards[j].email}</p><p>Github Username: ${engineerCards[j].github}</p>`;
-    };
-    var internString = ``;
+    }
+
+    if(engineerCards.length > 0) {
+    for (j=0;j<engineerCards.length;j++) {
+        console.log(engineerCards);
+        engineerString += `<div class="card" style="width: 18rem;"><div class="card-body"><h5 class="card-title">Engineer</h5><h6 class="card-subtitle mb-2 text-muted"> Name: ${engineerCards[j].name} </h6><p>Employee ID: ${engineerCards[j].id}</p><p>E-mail: ${engineerCards[j].email}</p><p>Github Username: ${engineerCards[j].github} </p>`;
+    };}
+
+    if(internCards.length > 0) {
     for (k=0;k<internCards.length;k++) {
-        engineerString += `<div class="card" style="width: 18rem;"><div class="card-body"><h5 class="card-title">Intern</h5><h6 class="card-subtitle mb-2 text-muted"> Name: ${internCards[k].name} </h6><p>Employee ID: ${internCards[k].id}</p><p>E-mail: ${internCards[k].email}</p><p>School: ${internCards[k].school}</p>`;
+        console.log(internCards);
+        internString += `<div class="card" style="width: 18rem;"><div class="card-body"><h5 class="card-title">Intern</h5><h6 class="card-subtitle mb-2 text-muted"> Name: ${internCards[k].name} </h6><p>Employee ID: ${internCards[k].id}</p><p>E-mail: ${internCards[k].email}</p><p>School: ${internCards[k].school} </p>`;
     };
+}
     const directory = `<!DOCTYPE html>
     <html lang="en">
        <head>
@@ -117,7 +120,7 @@ function createDirectoryFinal() {
         }
         console.log("Success!");
     });
-}
+};
 
 collectTeamInfo();
 
