5ebd2add8c550dde21f33cd0901cd770f0cc6e9d
Matthew Wester <MatthewWester@live.com>
2020-02-08 17:22:07 -0700

    Made it easier to html dumps into main repo folder now

--- app.js
+++ app.js
@@ -114,7 +114,7 @@ function createDirectoryFinal() {
           <span class="navbar-brand mb-0 h1">Company Directory</span>
           </nav>` + managerString + engineerString + internString + `<script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script><script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js' integrity='sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1' crossorigin='anonymous'></script><script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js' integrity='sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM' crossorigin='anonymous'></script></body><footer class='wrapper'></footer></html>`;
     console.log(directory);
-    fs.appendFile("./output/directory.html", directory, err => {
+    fs.appendFile("./directory.html", directory, err => {
         if(err) {
             console.log(err);
         }
